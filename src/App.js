752c4152706d373d4828483781be63ec225359f8
Chi Nguyen <nglinhchi65@gmail.com>
2023-03-30 22:48:51 +0300

    make changes to the exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
+import { useRef } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import 'dayjs/locale/en-gb';
 
-function App() {
+function Todolist() {
+  const [todo, setTodo] = useState({description: '', date: '', priority:''});
+  const [todos, setTodos] = useState([]);
+  const gridRef = useRef();
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const addTodo = (event) => {
+    if(todo.date) {
+    const formattedDate = todo.date.format('DD-MM-YYYY');
+    setTodos([...todos, {...todo, date: formattedDate}]);
+    } else {
+    setTodos([...todos, todo]);
+    }
+  }
+
+  const columns = [
+    { field: "description", sortable: true, filter: true },
+    { field: "date", sortable: true, filter: true },
+    { field: "priority", sortable: true, filter: true,
+      cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'} }
+  ];
+
+  const deleteTodo = () => {
+    if(gridRef.current.getSelectedNodes().length > 0) {
+      setTodos(todos.filter((todo, index) =>
+            index !== gridRef.current.getSelectedNodes()[0].id))
+    }
+    else {
+      alert('Select row first');
+    }
+    
+  }
+
+  const handleDate = (date) => {
+    if (date) {
+      setTodo({
+        ...todo,
+        date: date,
+      });
+    } else {
+      setTodo({
+        ...todo,
+        date: null,
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='search-area'>
+        <h5>Todo List</h5>
+        <Stack direction="row" spacing={2}>
+          <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
+            <DatePicker
+              label="Date"
+              variant="standard"
+              name="date" value={todo.date}
+              onChange={handleDate}
+               /> 
+          </LocalizationProvider>
+
+          <TextField
+            label="Description"
+            variant="standard"
+            name="description" value={todo.description}
+            onChange={inputChanged} />
+
+          <TextField
+            label="Priority"
+            variant="standard"
+            name="priority" value={todo.priority}
+            onChange={inputChanged} />
+
+          <Button onClick={addTodo} variant="contained">Add</Button>
+          <Button onClick={deleteTodo} variant = "contained">Delete</Button>
+        </Stack>
+      </div>
+
+      <div className='ag-theme-material'
+            style = {{height: '700px', width: '70%'}}>
+          <AgGridReact
+            ref = {gridRef}
+            onGridReady={ params => gridRef.current = params.api }
+            rowSelection='single'
+            columnDefs={columns}
+            rowData={todos}
+            animateRows={true}
+            defaultColDef={{filter:true, floatingFilter:true}}>
+          </AgGridReact>
+
+      </div>
+
     </div>
   );
-}
+};
 
-export default App;
+export default Todolist;
